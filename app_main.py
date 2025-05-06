10dd6cb5fd7ae63f64e43a6955fb3dd4ff98d13d
cv4gh3tt1_HOME <ch2vaghetti@gmail.com>
2025-05-06 20:03:27 -0300

    adiciona classes Task e TodoApp com a estrutura inicial do aplicativo de tarefas

--- app_main.py
+++ app_main.py
@@ -1,6 +1,25 @@
 import flet as ft
 
-def main(page: ft.Page):
+# Classe para criar as tarefas
+class Task(ft.Column):
+    pass
+
+
+# Classe para Criar o Aplicativo de tarefas
+class TodoApp(ft.Column):
+    def build(self):
+        self.new_task = ft.TextField(
+            hint_text="Digite uma nova tarefa",
+            expand=True,
+            on_submit=self.add_task,
+        )
+    
+    def add_task(self, e): # Adiciona uma nova tarefa
+        pass
+
+
+# Função principal que inicializa a página
+def main(page: ft.Page): 
     page.title = "Minhas Tarefas"
     page.window.width = 600
     page.window.height = 650
@@ -8,8 +27,10 @@ def main(page: ft.Page):
     page.padding = ft.padding.only(left=20, right=20, top=20, bottom=20)
     page.theme_mode = ft.ThemeMode.LIGHT
 
-    page.add(ft.Text("Minhas Tarefas", size=20, weight=ft.FontWeight.BOLD))
-
+    
     page.update()
 
+    app = TodoApp() # Cria uma instância do aplicativo de tarefas
+    page.add(app) # Adiciona o aplicativo à página
+    
 ft.app(target=main)
