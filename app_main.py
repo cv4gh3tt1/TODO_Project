9c3e758f71721438f8b9a296ccfff39a38855801
cv4gh3tt1 <ch2vaghetti@gmail.com>
2025-05-08 08:32:42 -0300

    refatora a classe Task para incluir funcionalidades de edição e deleção de tarefas

--- app_main.py
+++ app_main.py
@@ -2,30 +2,56 @@ import flet as ft
 
 
 # Classe para criar as tarefas
-class Task(ft.Column): # Herda de ft.Column para criar uma coluna de tarefas
-    def __init__ (self, task_name, task_status_change, task_delete): # Inicializa a classe com os parâmetros necessários
+class Task(ft.Column):  # Herda de ft.Column para criar uma coluna de tarefas
+    def __init__(
+        self, task_name, task_status_change, task_delete
+    ):  # Inicializa a classe com os parâmetros necessários
         super().__init__()
         self.task_completed = False  # Inicializa o status da tarefa como não concluída
         self.task_name = task_name  # Nome da tarefa oriunda do textfield
-        self.task_status_change = task_status_change  # Função para alterar o status da tarefa
+        self.task_status_change = (
+            task_status_change  # Função para alterar o status da tarefa
+        )
         self.task_delete = task_delete  # Função para deletar a tarefa
 
         def build(self):  # Método para construir a tarefa
-            self.display_task = ft.Checkbox( #
-                value=False, # Inicializa o checkbox como não selecionado
-                label=self.task_name, # Nome da tarefa
-                on_change=self.task_status_change, # Função para alterar o status da tarefa
+            self.display_task = ft.Checkbox(  #
+                value=False,  # Inicializa o checkbox como não selecionado
+                label=self.task_name,  # Nome da tarefa
+                on_change=self.task_status_change,  # Função para alterar o status da tarefa
             )
 
-            self.edit_name = ft.TextField(expand=True, on_submit=self.clicked) # Campo de texto para editar o nome da tarefa
+            self.edit_name = ft.TextField(
+                expand=True, on_submit=self.clicked
+            )  # Campo de texto para editar o nome da tarefa
 
-            self.display_view (ft.Row( # Cria uma linha para exibir a tarefa
-                controls=[
+            self.display_view(
+                ft.Row(  # Cria uma linha para exibir a tarefa
+                    controls=[
+                        self.display_task,  # Checkbox para marcar a tarefa como concluída
+                        ft.Row(
+                            controls=[
+                                ft.IconButton(
+                                    icon=ft.Icons.CREATE_OUTLINED,  # Ícone para editar a tarefa
+                                    icon_color=ft.Colors.GREEN,  # Cor do ícone
+                                    tooltip="Editar tarefa",  # Dica de ferramenta ao passar o mouse
+                                    on_click=self.edit_clicked,  # Função chamada ao clicar no ícone de editar
+                                ),
+                                ft.IconButton(
+                                    icon=ft.Icons.DELETE_OUTLINED,  # Ícone para deletar a tarefa
+                                    icon_color=ft.Colors.RED,  # Cor do ícone
+                                    tooltip="Deletar tarefa",  # Dica de ferramenta ao passar o mouse
+                                    on_click=self.delete_clicked,  # Função chamada ao clicar no ícone de deletar
+                                ),
+                            ]
+                        ),
+                    ]
+                ),
+            )
 
-                ]),)
-
-    def save_cliced(self, e): # # Método para salvar a tarefa clicada
+    def save_cliced(self, e):  # # Método para salvar a tarefa clicada
         pass
+
 
 # Classe para Criar o Aplicativo de tarefas
 class TodoApp(ft.Column):
@@ -33,34 +59,36 @@ class TodoApp(ft.Column):
         super().__init__()
         self.controls = self.build()  # Chama o método build e adiciona os controles
 
-    def build(self):
-        self.new_task = ft.TextField(
+    def build(self):  # Método para construir o aplicativo de tarefas
+        self.new_task = ft.TextField(  # Campo de texto para inserir uma nova tarefa
             hint_text="Digite uma nova tarefa",
             expand=True,
             on_submit=self.add_task,
         )
 
-        self.filter = ft.Tabs(
+        self.filter = ft.Tabs(  # Filtro de tarefas
             scrollable=False,
             selected_index=0,
-            on_change=self.tabs_changed,
-            tabs=[
+            on_change=self.tabs_changed,  # Função chamada ao mudar de aba
+            tabs=[  # Lista de abas para filtrar as tarefas
                 ft.Tab(text="Todas"),
                 ft.Tab(text="Ativas"),
                 ft.Tab(text="Concluídas"),
             ],
         )
 
-        self.items_left = ft.Text("0 tarefas restantes")
+        self.items_left = ft.Text(
+            "0 tarefas restantes"
+        )  # Texto para mostrar o número de tarefas restantes
 
-        return [
-            # Titulo do App
+        return [  # Lista de controles do aplicativo
+            # Cabeçalho do aplicativo
             ft.Row(
                 [
                     ft.Text(
                         value="Tarefas",
                         size=30,
-                        color=ft.colors.with_opacity(0.7, "black"),
+                        color=ft.Colors.with_opacity(0.7, "black"),
                     )
                 ],
                 alignment="center",
@@ -69,15 +97,16 @@ class TodoApp(ft.Column):
             ft.Row(
                 controls=[
                     self.new_task,
-                    ft.FloatingActionButton(icon=ft.icons.ADD, on_click=self.add_task),
+                    ft.FloatingActionButton(
+                        icon=ft.Icons.ADD, on_click=self.add_task
+                    ),  # Botão para adicionar nova tarefa
                 ],
             ),
             # Lista de Tarefas
             ft.Column(
                 controls=[
                     self.filter,  # Filtro de tarefas
-                    # self.tasks,  # Placeholder para as tarefas
-                    ft.Row(
+                    ft.Row(  # # Linha para exibir as tarefas
                         alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
                         vertical_alignment=ft.CrossAxisAlignment.CENTER,
                         controls=[
@@ -85,8 +114,8 @@ class TodoApp(ft.Column):
                             ft.OutlinedButton(
                                 text="Apagar tarefas concluidas".upper(),
                                 on_click=self.clear_completed_tasks,
-                                icon=ft.icons.DELETE,
-                                icon_color=ft.colors.RED_400,
+                                icon=ft.Icons.DELETE,
+                                icon_color=ft.Colors.RED_400,
                             ),
                         ],
                     ),
